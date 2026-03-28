4d90e619863b664a6075a47920e4a2735d85fde7
Emil Fredriksson <fredrikssonemil@hotmail.com>
2026-03-28 16:41:25 +0100

    chore: add Teal (tl) type definitions from the language server

---
+++ tlconfig.lua
@@ -0,0 +1,13 @@
+return {
+   global_env_def = "engine.lutro",
+   include_dir = {"./src", "./src/formatter"},
+   warning_error = {
+      "branch",
+      "debug",
+      "hint",
+      "redeclaration",
+      "unknown",
+      "unread",
+      "unused",
+   },
+}
